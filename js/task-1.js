9ab8799a14cfd7216aabd15c4a31a0210ace66fb
Roman Palna <roma.palna@gmail.com>
2020-09-19 10:54:41 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,6 @@
 const categoryEL = document.querySelector('#categories');
 console.log(`У списку ${categoryEL.children.length} категорії.`);
 
-// const titleEl = document
-//   .querySelectorAll('h2')
-//   .forEach(title => console.log(`Категорія: ${title.textContent}`));
-
-// const titleElLenght = document
-//   .querySelectorAll('.item ul')
-//   .forEach(li => console.log(`Кількість елементів: ${li.children.length}`));
-
 const categoryItems = document.querySelectorAll('.item');
 categoryItems.forEach(item => {
   console.log(`Категорія: ${item.querySelector('h2').textContent}`);
